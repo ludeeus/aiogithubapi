f07892a0d40d288655e38ece18b6b45dd2584584
Ludeeus <ludeeus@ludeeus.dev>
2020-09-13 13:25:57 +0000

    test session

--- tests/device/test_device_login.py
+++ tests/device/test_device_login.py
@@ -1,7 +1,15 @@
+import aiohttp
 from tests.common import load_fixture
 from tests.const import NOT_RATELIMITED
 import pytest
 from aiogithubapi import GitHubDevice
+
+
+@pytest.mark.asyncio
+async def test_session():
+    async with aiohttp.ClientSession() as session:
+        device = GitHubDevice("xxxxx", "", session)
+        assert device.session == session
 
 
 @pytest.mark.asyncio
