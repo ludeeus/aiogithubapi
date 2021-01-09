6fb01cf91e75725b13c7d13f888f258c0dfd949a
Ludeeus <ludeeus@ludeeus.dev>
2021-01-09 19:58:00 +0000

    fix fixture

--- tests/common.py
+++ tests/common.py
@@ -4,7 +4,7 @@ import os
 
 def load_fixture(filename, asjson=False):
     """Load a fixture."""
-    filename = f"{filename}.json" if asjson and ".json" not in filename else filename
+    filename = f"{filename}.json" if "." not in filename else filename
     path = os.path.join(os.path.dirname(__file__), "fixtures", filename)
     with open(path, encoding="utf-8") as fptr:
         if asjson:
