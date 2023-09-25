9a33a95f48fa10d5be63c861c86aa190eaffb4a8
ludeeus <joasoe@gmail.com>
2023-09-25 19:07:12 +0000

    Add helper to verify release assets with sigstore

--- aiogithubapi/helpers.py
+++ aiogithubapi/helpers.py
@@ -1,9 +1,17 @@
 """Helpers for AIOGitHubAPI."""
 from __future__ import annotations
 
+from io import BytesIO
 from typing import Optional
 
 import aiohttp
+from sigstore.verify import (
+    VerificationMaterials,
+    VerificationResult,
+    Verifier,
+    models,
+    policy,
+)
 
 from .const import HttpMethod, Repository, RepositoryType
 from .legacy.helpers import (
@@ -36,4 +44,41 @@ async def async_call_api(
     """Deprecated: Execute the API call."""
     return await legacy_async_call_api(
         session, method, url, headers, params, data, jsondata, returnjson
+    )
+
+
+def sigstore_verify_downloaded_release_asset(
+    asset: bytes,
+    signature_bundle: bytes,
+    repository: str,
+    workflow: str,
+    tag: str,
+    *,
+    workflow_name: str | None = None,
+    workflow_trigger: str | None = None,
+    offline_verification: bool = False,
+    **kwargs,
+) -> VerificationResult:
+    """Verify release asset."""
+    verifier = Verifier.production()
+    policies = [
+        policy.Identity(
+            identity=f"https://github.com/{repository}/.github/workflows/{workflow}@refs/tags/{tag}",
+            issuer="https://token.actions.githubusercontent.com",
+        ),
+        policy.GitHubWorkflowRepository(repository),
+        policy.GitHubWorkflowRef(f"refs/tags/{tag}"),
+    ]
+    if workflow_trigger:
+        policies.append(policy.GitHubWorkflowTrigger(workflow_trigger))
+    if workflow_name:
+        policies.append(policy.GitHubWorkflowName(workflow_name))
+
+    return verifier.verify(
+        VerificationMaterials.from_bundle(
+            input_=BytesIO(asset),
+            bundle=models.Bundle().from_json(signature_bundle),
+            offline=offline_verification,
+        ),
+        policy=policy.AllOf(policies),
     )
--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,6 +1,10 @@
 """Test repository dataclass."""
+from io import BytesIO
+import json
+from unittest.mock import patch
 from aiogithubapi import Repository
-from aiogithubapi.helpers import repository_full_name
+from aiogithubapi.helpers import repository_full_name, sigstore_verify_downloaded_release_asset
+from sigstore.verify import VerificationSuccess, VerificationFailure
 
 
 def test_repository():
@@ -13,3 +17,36 @@ def test_repository():
     assert (
         repository_full_name({"owner": "octocat", "repo": "Hello-World"}) == "octocat/Hello-World"
     )
+
+
+def test_sigstore_success():
+    with patch("aiogithubapi.helpers.Verifier.verify", return_value=VerificationSuccess()), patch(
+        "aiogithubapi.helpers.VerificationMaterials.from_bundle"
+    ):
+        verification = sigstore_verify_downloaded_release_asset(
+            asset=b"test",
+            signature_bundle=bytes(json.dumps({"critical": {"identity": "test"}}), "utf-8"),
+            repository="test",
+            workflow="test",
+            tag="test",
+        )
+        assert verification.success
+        assert "reason" not in verification.json()
+        assert verification.json() == json.dumps({"success": True})
+
+
+def test_sigstore_failure():
+    with patch(
+        "aiogithubapi.helpers.Verifier.verify",
+        return_value=VerificationFailure(reason="Some reason"),
+    ), patch("aiogithubapi.helpers.VerificationMaterials.from_bundle"):
+        verification = sigstore_verify_downloaded_release_asset(
+            asset=b"test",
+            signature_bundle=bytes(json.dumps({"critical": {"identity": "test"}}), "utf-8"),
+            repository="test",
+            workflow="test",
+            tag="test",
+        )
+        assert not verification.success
+        assert verification.reason == "Some reason"
+        assert verification.json() == json.dumps({"success": False, "reason": "Some reason"})
