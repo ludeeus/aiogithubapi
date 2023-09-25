8f9f60635ed2770de551ef0857428539efd9a35d
Joakim Sørensen <ludeeus@ludeeus.dev>
2023-09-25 22:23:54 +0200

    Add helper to verify release assets with sigstore (#229)
    * Add helper to verify release assets with sigstore
    
    * rename
    
    * add all options to test

--- tests/test_helper.py
+++ tests/test_helper.py
@@ -1,6 +1,10 @@
 """Test repository dataclass."""
+from io import BytesIO
+import json
+from unittest.mock import patch
 from aiogithubapi import Repository
-from aiogithubapi.helpers import repository_full_name
+from aiogithubapi.helpers import repository_full_name, sigstore_verify_release_asset
+from sigstore.verify import VerificationSuccess, VerificationFailure
 
 
 def test_repository():
@@ -13,3 +17,40 @@ def test_repository():
     assert (
         repository_full_name({"owner": "octocat", "repo": "Hello-World"}) == "octocat/Hello-World"
     )
+
+
+def test_sigstore_success():
+    with patch("aiogithubapi.helpers.Verifier.verify", return_value=VerificationSuccess()), patch(
+        "aiogithubapi.helpers.VerificationMaterials.from_bundle"
+    ):
+        verification = sigstore_verify_release_asset(
+            asset=b"test",
+            signature_bundle=bytes(json.dumps({"critical": {"identity": "test"}}), "utf-8"),
+            repository="test",
+            workflow="test",
+            tag="test",
+            workflow_name="test",
+            workflow_trigger="release",
+        )
+        assert verification.success
+        assert "reason" not in verification.json()
+        assert verification.json() == json.dumps({"success": True})
+
+
+def test_sigstore_failure():
+    with patch(
+        "aiogithubapi.helpers.Verifier.verify",
+        return_value=VerificationFailure(reason="Some reason"),
+    ), patch("aiogithubapi.helpers.VerificationMaterials.from_bundle"):
+        verification = sigstore_verify_release_asset(
+            asset=b"test",
+            signature_bundle=bytes(json.dumps({"critical": {"identity": "test"}}), "utf-8"),
+            repository="test",
+            workflow="test",
+            tag="test",
+            workflow_name="test",
+            workflow_trigger="release",
+        )
+        assert not verification.success
+        assert verification.reason == "Some reason"
+        assert verification.json() == json.dumps({"success": False, "reason": "Some reason"})
